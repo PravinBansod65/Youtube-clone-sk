107c63253f5c396917bf912103aa59cf7af3678f
silentSK987 <pravinbansod65@gmail.com>
2024-02-04 11:33:28 +0530

    Youtube clone done

---
+++ script.js
@@ -0,0 +1,89 @@
+
+
+const API_KEY = "AIzaSyD1DEQ8pB2Tq31jiGRduAD3hKc_Myi1AZo";
+// const API_KEY = "AIzaSyAO-TYd2kVQtS0J1gti4sCmDuQ81-S5m-c";
+//// backup key =>"AIzaSyCkKhuzGS5vmf_wreWgVMdhsy-z8teYJeY"
+const BASE_URL = "https://www.googleapis.com/youtube/v3";
+let channelHttp = "https://www.googleapis.com/youtube/v3/channels?";
+// * --------------------GETTING VIDEOS---------------------------------------
+
+function getVideos(searchValue) {
+  let url;
+  url = `${BASE_URL}/search?key=${API_KEY}&q=${searchValue}&type=video&part=snippet&maxResults=20`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const videosContainer = document.getElementById("videos-container");
+      videosContainer.innerHTML = "";
+      data.items.forEach((item) => {
+        getChannelIcon(item);
+      });
+    });
+}
+
+// *---------------- GEttting channel ICON ---------  --------
+
+const getChannelIcon = (video) => {
+  fetch(
+    `${BASE_URL}/channels?key=${API_KEY}&part=snippet&part=statistics&id=${video.snippet.channelId}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      video.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+
+      // console.log(video.channelThumbnail + " thumbnail taken"); //
+      display(video);
+    });
+  // .catch((error) => console.error("Error fetching channel data:", error));
+};
+
+// *---------------- Displaying videos-----------------
+function display(videos) {
+  const videosContainer = document.getElementById("videos-container");
+  // console.log(videos , "display videos link");  //
+
+  videosContainer.innerHTML += `
+        <div class="video" id="videos-container">
+        <a href="/video.html?videoId=${videos.id.videoId}">
+        <img src="${videos.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        <div class="content"> 
+        <img src="${videos.channelThumbnail}" class="channel-icon" alt="">
+              <div class="info">
+              <h4 class="title">${videos.snippet.title}</h4>
+              <p class="channel-name">${videos.snippet.channelTitle}</p>
+              </div>
+              </div>
+              </a>
+              </div>
+              `;
+}
+
+// <a href="/video.html?videoId=${videos.id.videoId}">
+//     <img src="${videos.snippet.thumbnails.high.url}">
+//     <p>${videos.snippet.description}<p>
+// </a>
+//  * -----------calling function nd searching---------------------------
+getVideos("");
+document.getElementById("search-btn").addEventListener("click", () => {
+  // getVideos(document.getElementById("search-input").value);
+  const searchInputValue = document.getElementById("search-input").value;
+  getVideos(searchInputValue);
+});
+
+// todo --------------hovering on button--------------------------------
+const buttons = document.querySelectorAll('.hover-button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    console.log(button.innerHTML);
+    getVideos(button.innerHTML);
+    button.classList.toggle('green');
+    // button.classList.remove('green');
+  });
+  
+  // button.addEventListener('c', function() {
+  //   button.classList.remove('green');
+  // });
+});
+
---
+++ video.js
@@ -0,0 +1,130 @@
+const API_KEY = "AIzaSyD1DEQ8pB2Tq31jiGRduAD3hKc_Myi1AZo";
+// AIzaSyALato09pGN6xcaDRjCE7TEi33h-dOgz1Q
+// const API_KEY = "https://mocki.io/v1/94fe6938-229a-4e84-853f-fc6b64022382";
+const BASE_URL = "https://www.googleapis.com/youtube/v3";
+
+// todo------------------------- Loading content --------------------------------------
+window.addEventListener("load", () => {
+  const search = window.location.search;
+  const params = new URLSearchParams(search);
+
+  const videoId = params.get("videoId");
+  //    or we can get the videoid using localStorage.getItem
+
+  if (YT) {
+    new YT.Player("video-container", {
+      height: "420",
+      width: "770",
+      videoId: videoId,
+    });
+  }
+
+  function getRelatedContent(channelTitle) {
+    let url;
+    url = `${BASE_URL}/search?key=${API_KEY}&q=${channelTitle}&type=video&part=snippet&maxResults=1`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        display(data.items);
+      });
+  }
+
+  function getVideosDetails() {
+    let url;
+    url = `${BASE_URL}/videos?key=${API_KEY}&part=snippet&id=${videoId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        document.getElementById("video_title").innerHTML = `
+        ${data.items[0].snippet.title}
+        `;
+        console.log(data.items[0].snippet.channelId);
+        // *
+        getChannelDetails(data.items[0].snippet.channelId);
+        getRelatedContent(data.items[0].snippet.channelTitle);
+      });
+  }
+
+  function getChannelDetails(channel) {
+    let url;
+    url = `${BASE_URL}/channels?key=${API_KEY}&part=snippet&part=statistics&id=${channel}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        document.getElementById("channelInfo").innerHTML = `
+              <img src="${data.items[0].snippet.thumbnails.high.url}" class="channel-icon" alt="">
+              <h5 class="channelName">${data.items[0].snippet.title}</h5>
+              <p style="color:grey">${data.items[0].statistics.subscriberCount} Subscribers</p>
+        `;
+        document.getElementById("description").innerHTML = `
+          <p>${data.items[0].snippet.localized.description} </p>
+          `;
+      });
+  }
+
+  function getComments() {
+    let url;
+    url = `${BASE_URL}/commentThreads?key=${API_KEY}&part=snippet&videoId=${videoId}&maxResults=10`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        var cmnt;
+        data.items
+          .map((comment) => {
+            cmnt += `<img src="${comment.snippet.topLevelComment.snippet.authorProfileImageUrl}" class="profile-img">
+          <span>${comment.snippet.topLevelComment.snippet.authorDisplayName}</span>
+        
+          <p>${comment.snippet.topLevelComment.snippet.textOriginal}</p>
+          `;
+          })
+          .join("");
+
+        document.getElementById("other_comments").innerHTML = cmnt;
+      });
+  }
+
+  function display(videos) {
+    var myDiv = document.querySelector(".myDiv");
+    var content = myDiv.innerHTML; // This will give you the HTML content inside the <div>
+
+    const htmlContent = videos
+      .map(
+        (video) =>
+          `
+        <a href="/video.html?videoId=${video.id.videoId}">
+          <li>
+            <img src="${video.snippet.thumbnails.high.url}">
+            <p>${video.snippet.description}</p>
+          </li>
+        </a>
+        `
+      )
+      .join("");
+
+    myDiv.innerHTML += htmlContent;
+  }
+
+  getVideosDetails(); // ! main callling
+  getComments();
+});
+
+
+// todo ------------------- loading content  end--------------------------------------
+/*
+items: Array(1)
+ =>0: etag: "QTK8KAwM9aWgXum_Vlkr1BiXhQk"
+   id: "RZV4oXuTZlA"
+   kind: "youtube#video"
+     =>snippet: categoryId: "24"
+        channelId: "UCtf9cFBJkHVAf2qMqF01xYg"
+        channelTitle: "Hey Bear Sensory"
+        defaultAudioLanguage: "en-GB"
+        defaultLanguage: "en-GB"
+        description: "Welcome to Hey Bear Sensory! \n\nHere it is - EVERY Fruit and Veggie video back to back on one stream (apart from the seasonal two!)!\nEnjoy!\n\nKey features of this video\n\n- Bright music with lots of rhythm to encourage movement\n- Builds positive relationships with healthy food\n- Friendly characters\n- Bright colours for tracking"
+        liveBroadcastContent: "live"
+        maxres :"url://"
+*/
